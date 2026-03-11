1708c4c714122d40465df22dc23d7c8810363cd3
huynhdieu783 <huynhdieu783@gmail.com>
2026-03-11 15:15:08 +0700

    Delete THU directory

--- THU/timkiem.py
+++
@@ -1,9 +0,0 @@
-def trans(text):
-    text = text.lower()
-    inputs = "áàảãạăắằẳẵặâấầẩẫậéèẻẽẹêếềểễệíìỉĩịóòỏõọôốồổỗộơớờởỡợúùủũụưứừửữựýỳỷỹỵđ"
-    outputs = "aaaaaaaaaaaaaaaaaeeeeeeeeeeeiiiiiooooooooooooooooouuuuuuuuuuuyyyyyd"
-    dich = text.maketrans(inputs, outputs)
-    return text.translate(dich)
-noi_dung = input("Nhập chuỗi tiếng Việt có dấu: ")
-ket_qua = trans(noi_dung)
-print(f"Kết quả sau khi chuyển đổi: {ket_qua}")
